a07bf5053d6a86aa56f36e105b9c8249a93353a2
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-04-17 16:35:24 -0400

    Add logout button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavLink } from "react-router-dom";
+import { fire } from "../../config/firebase";
 
 import Logo from "../../assets/logo.png";
-
-import Info from "../../assets/info_icon.png";
-
 import Back from "../../assets/back_icon.png";
 
-import "./styles.css";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
-const Header = ({ title }) => (
-  <div className="header">
-    <header>
-      {title === "Sobre Nós" ? (
-        <NavLink to="/" className="back">
-          <img src={Back} id="back" alt="Back to Home Screen" />
-        </NavLink>
-      ) : (
-        <br />
-      )}
-      <img src={Logo} alt="" />
-      <h1>{title}</h1>
-      {title === "Eventos" ? (
-        <NavLink to="/about" className="info">
-          <img src={Info} id="info" alt="About Us" />
-        </NavLink>
-      ) : (
-        <br />
-      )}
-    </header>
-  </div>
-);
+import "./styles.css";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Button,
+  List,
+  ListItem,
+} from "@material-ui/core";
+
+const Header = ({ title }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("uid");
+
+    if (fire.auth().currentUser) {
+      fire.auth().signOut();
+    }
+
+    window.location.reload();
+    handleClose();
+  };
+
+  return (
+    <div className="header">
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <List>
+          <ListItem>
+            <NavLink to="/about" className="info">
+              <button onClick={handleClose} className="dialogButton">
+                Sobre nós
+              </button>
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <button
+              onClick={handleLogout}
+              className="dialogButton"
+              style={{ background: "#043f5f" }}
+            >
+              Log out
+            </button>
+          </ListItem>
+        </List>
+      </Dialog>
+
+      <header>
+        {title === "Sobre Nós" ? (
+          <NavLink to="/" className="back">
+            <img src={Back} id="back" alt="Back to Home Screen" />
+          </NavLink>
+        ) : (
+          <br />
+        )}
+        <img src={Logo} alt="" />
+        <h1>{title}</h1>
+        {title === "Eventos" ? (
+          <div className="options">
+            <MoreVertIcon style={{ fontSize: 40 }} onClick={handleClickOpen} />
+          </div>
+        ) : (
+          <br />
+        )}
+      </header>
+    </div>
+  );
+};
 
 export default Header;
